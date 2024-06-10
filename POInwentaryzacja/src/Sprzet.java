7b2bc36bb27fdba9d1429c692c28f13181acc614
domii0102 <161345720+domii0102@users.noreply.github.com>
2024-06-10 22:00:25 +0200

    koniec

--- POInwentaryzacja/src/Sprzet.java
+++ POInwentaryzacja/src/Sprzet.java
@@ -1,28 +1,30 @@
 import java.time.LocalDate;
 
 import static java.time.LocalDate.parse;
-
+// Klasa abstrakcyjna Sprzet
 public abstract class Sprzet extends Przedmiot{
     private static final long  serialVersionUID = 1L;
     private LocalDate ostatniaKonserwacja;
     private final String numerSeryjny;
     private String producent;
-
+    // Konstruktor klasy Sprzet
     protected Sprzet(String nazwa,String data,String stanPrzedmiotu,String numerSeryjny,String producent,String ostatniaKonserwacja){
         super(nazwa,data,stanPrzedmiotu);
         this.numerSeryjny = numerSeryjny;
         setProducent(producent);
         setOstatniaKonserwacja(ostatniaKonserwacja);
     }
+    // Konstruktor kopiujący
     protected Sprzet(Sprzet s){
         super(s);
         this.numerSeryjny = s.numerSeryjny;
         this.producent = s.producent;
         this.ostatniaKonserwacja = s.ostatniaKonserwacja;
     }
+    // Metoda abstrakcyjna do klonowania obiektu
     @Override
     protected abstract Sprzet clone();
-
+    // Metoda ustawiająca producenta, sprawdza poprawność formatu
     public void setProducent(String producent){
         if(!producent.matches("^[A-Za-z]*$")){
             throw new IllegalArgumentException("Nazwa producenta składa się tylko z liter");
@@ -32,7 +34,7 @@ public abstract class Sprzet extends Przedmiot{
         }
 
     }
-
+    // Metoda ustawiająca datę ostatniej konserwacji, sprawdza poprawność formatu
     public void setOstatniaKonserwacja(String ostatniaKonserwacja) {
         if(!ostatniaKonserwacja.matches("^(?:(?!0000)\\d{4})-(?:(?:0[1-9]|1[0-2]))-(?:(?:0[1-9]|1\\d|2\\d|3[01]))$")){
             throw new IllegalArgumentException("Podano zły format daty");
