96aa0fb036aabebb6da3ccc1d73fc1630e68f22a
domii0102 <161345720+domii0102@users.noreply.github.com>
2024-06-10 21:42:31 +0200

    koniec

--- POInwentaryzacja/src/Mebel.java
+++ POInwentaryzacja/src/Mebel.java
@@ -3,38 +3,39 @@ public abstract class Mebel extends Przedmiot{
     private int szerokosc;
     private int wysokosc;
     private int dlugosc;
-
+    // Konstruktor inicjalizujący obiekt Mebel
     public Mebel(String nazwa,String data,String stanPrzedmiotu, String szerokosc,String wysokosc,String dlugosc){
         super(nazwa, data, stanPrzedmiotu);
         setSzerokosc(szerokosc);
         setWysokosc(wysokosc);
         setDlugosc(dlugosc);
     }
+    // Konstruktor kopiujący
     public Mebel(Mebel m){
         super(m);
     }
-
+    // Ustawia szerokość mebla po sprawdzeniu poprawności formatu
     public void setSzerokosc(String szerokosc) {
         if (!isInt(szerokosc)) {
             throw new IllegalArgumentException("Szerkość musi być liczbą");
         }
         this.szerokosc = Integer.parseInt(szerokosc);
     }
-
+    // Ustawia wysokość mebla po sprawdzeniu poprawności formatu
     public void setWysokosc(String wysokosc) {
         if(!isInt(wysokosc)){
             throw new IllegalArgumentException("Wysokość musi być liczbą");
         }
         this.wysokosc = Integer.parseInt(wysokosc);
     }
-
+    // Ustawia długość mebla po sprawdzeniu poprawności formatu
     public void setDlugosc(String dlugosc) {
         if(!isInt(dlugosc)){
             throw new IllegalArgumentException("Długość musi być liczbą");
         }
         this.dlugosc = Integer.parseInt(dlugosc);
     }
-
+    // Abstrakcyjna metoda klonująca
     @Override
     protected abstract Mebel clone();
     private boolean isInt(String text){
